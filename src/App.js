9004e8ace10d1d685f6ea9aa1ebe3230637ff587
Mauricio Orozco <maudob@live.com>
2019-07-16 14:21:15 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>"Mi primera aplicación"</h1>
+          <p>"Esta es mi primera aplicación en React, está padrísimo!"</p>
+      </div>
+    );
+  }
+}
+
+export default App;
