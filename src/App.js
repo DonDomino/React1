6ba8fd480b07ad5c3ca7b3a8514550512f9433e7
Mauricio Orozco <maudob@live.com>
2019-07-16 14:24:19 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>"Mi primera aplicación"</h1>
-          <p>"Esta es mi primera aplicación en React, está padrísimo!"</p>
+        <h1>Mi primera aplicación</h1>
+          <p>Esta es mi primera aplicación en React, está padrísimo!</p>
       </div>
     );
   }
